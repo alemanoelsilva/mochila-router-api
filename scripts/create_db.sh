6dd9d4a7ff271f70e89c160a19d182aac76f32c5
Alexandre Manoel da Silva <alemanoelsilva@ales-pro.local>
2018-07-15 23:55:08 -0300

    configuration image docker the of postgres

---
+++ scripts/create_db.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+POSTGRES="psql --username postgres"
+
+echo "Creating database mochilarouter"
+
+$POSTGRES <<EOSQL
+CREATE DATABASE mochilarouter OWNER postgres;
+EOSQL
+
+echo "Creating database mochilarouter_test"
+
+$POSTGRES <<EOSQL
+CREATE DATABASE mochilarouter_test OWNER postgres;
+EOSQL
