8a54c601cfa6faf00e12af91f8dc4e971eccb892
Alexandre Manoel da Silva <alemanoelsilva@gmail.com>
2018-12-31 20:47:58 +0000

    implementation test from list repository

--- api/itineraries/repository.integration.spec.js
+++ api/itineraries/repository.integration.spec.js
@@ -3,24 +3,16 @@
 const repository = require('./repository');
 const Itinerary = require('./model');
 
-const { database } = require('../../test/helpers');
-
-const itinerary = {
-  name: 'name for test',
-  description: 'description for test',
-  duration: 5,
-  isPrivate: false,
-  user: 'user for test',
-  places: [{
-    name: 'name place for test',
-    attraction: 'attraction place for test',
-  }],
-};
+const {
+  database,
+  payloadMock: itinerary,
+  listOfItinerariesMock,
+} = require('../../test/helpers');
 
 beforeEach(async () => database(Itinerary).destroy());
 
 describe('Itinerary Repository Integration Test', () => {
-  describe('Creation of itinerary with success', () => {
+  describe('Creating Repository', () => {
     test('Should create one itinerary', async () => {
       try {
         const { create } = repository(Itinerary);
@@ -41,20 +33,55 @@ describe('Itinerary Repository Integration Test', () => {
           expect(place).toHaveProperty('name', payload.places[index].name);
           expect(place).toHaveProperty('attraction', payload.places[index].attraction);
         });
-      } catch (error) {}
+      } catch (error) { }
     });
-  });
 
-  describe('Creation of itinerary with no success', () => {
-    test('Should try create one itinerary', async () => {
+    test('Should return error when field name is null', async () => {
       try {
         const { create } = repository(Itinerary);
 
-        await create({ ...itinerary, name: null});
+        await create({ ...itinerary, name: null });
 
       } catch (error) {
         expect(error).toHaveProperty('name', 'SequelizeValidationError');
         expect(error).toHaveProperty('message', 'notNull Violation: itinerary.name cannot be null');
+      }
+    });
+  });
+
+  describe('Listing Repository', () => {
+    beforeEach(async () => database(Itinerary).createMany(listOfItinerariesMock));
+
+    test('Should list all itineraries', async () => {
+      try {
+        const { getAll } = repository(Itinerary);
+
+        const { itineraries, count } = await getAll();
+
+        expect(count).toEqual(3);
+        expect(itineraries.length).toEqual(3);
+
+        itineraries.forEach((itinerary, index) => {
+          expect(itinerary.isPrivate).toEqual(listOfItinerariesMock[index].isPrivate);
+          expect(itinerary.isActive).toEqual(listOfItinerariesMock[index].isActive);
+          expect(itinerary.name).toEqual(listOfItinerariesMock[index].name);
+          expect(itinerary.description).toEqual(listOfItinerariesMock[index].description);
+          expect(itinerary.duration).toEqual(listOfItinerariesMock[index].duration);
+          expect(itinerary.user).toEqual(listOfItinerariesMock[index].user);
+          expect(itinerary.places).toEqual(listOfItinerariesMock[index].places);
+        });
+      } catch (error) { }
+    });
+
+    test('Should return error when the filter contains fields that does not exist on Schema from Database', async () => {
+      try {
+        const { getAll } = repository(Itinerary);
+
+        await getAll({ limit: 1, page: 1, other_field: null });
+
+      } catch (error) {
+        expect(error).toHaveProperty('name', 'SequelizeDatabaseError');
+        expect(error).toHaveProperty('message', 'column itinerary.other_field does not exist');
       }
     });
   });
