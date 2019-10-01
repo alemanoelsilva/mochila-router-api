24a709a35346fae937c5d86ea6eac1a2848d6da4
Alexandre Manoel da Silva <alemanoelsilva@gmail.com>
2019-09-30 23:33:28 -0300

    new commit

--- config/sequelize.js
+++ config/sequelize.js
@@ -9,6 +9,7 @@ let connection = null;
 const sequelizeDB = ({
   connect: async ({ database, user, pass }) => {
     if (connection) return connection;
+
     try {
       connection = new Sequelize(database, user, pass, {
         host: 'localhost',
